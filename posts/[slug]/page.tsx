66553f7c1583f679f652f04a3b0fce5690cd7566
Karlie Zhao <62089883+KarlieZhao@users.noreply.github.com>
2024-09-15 14:07:42 -0400

    Update page.tsx

--- posts/[slug]/page.tsx
+++ posts/[slug]/page.tsx
@@ -3,11 +3,8 @@ import { notFound } from "next/navigation";
 import { getAllPosts, getPostBySlug } from "@/lib/api";
 import { CMS_NAME } from "@/lib/constants";
 import markdownToHtml from "@/lib/markdownToHtml";
-import Alert from "@/app/_components/alert";
 import Container from "@/app/_components/container";
 import Header from "@/app/_components/header";
-import { PostBody } from "@/app/_components/post-body";
-import { PostHeader } from "@/app/_components/post-header";
 
 export default async function Post({ params }: Params) {
   const post = getPostBySlug(params.slug);
@@ -20,17 +17,16 @@ export default async function Post({ params }: Params) {
 
   return (
     <main>
-      <Alert preview={post.preview} />
       <Container>
         <Header />
         <article className="mb-32">
-          <PostHeader
+          {/* <PostHeader
             title={post.title}
             coverImage={post.coverImage}
             date={post.date}
             author={post.author}
           />
-          <PostBody content={content} />
+          <PostBody content={content} /> */}
         </article>
       </Container>
     </main>
