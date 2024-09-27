3c3dde4fdb436634866189d0dc9a13d7716b7618
Karlie Zhao <62089883+KarlieZhao@users.noreply.github.com>
2024-09-27 13:51:13 -0400

    mapembed layout

--- src/app/_components/mapembed.tsx
+++ src/app/_components/mapembed.tsx
@@ -5,15 +5,23 @@ import { useWindowWidth } from "@/lib/resize"; // Adjust the import path as nece
 import { useWindowHeight } from "@/lib/resize"; // Adjust the import path as necessary
 
 export function Map() {
-  const windowWidth = useWindowWidth();
-  const frameHeight = useWindowHeight()*1.05;
+  // const windowWidth = useWindowWidth();
+  const frameHeight = useWindowHeight() * 0.97;
+
+  function preventZoom(e: React.WheelEvent<HTMLIFrameElement>) {
+    e.stopPropagation();
+  }
 
   return (
-    <section className="w-full" style={{ height: `${frameHeight}px` }}>
-      <iframe 
-        className="w-full h-full border-0" 
-        title="Test_Karlie" 
-        src="//tudelft.maps.arcgis.com/apps/Embed/index.html?webmap=e788f61aa7aa49dca367c8db8d934271&extent=34.6218,32.9125,36.4579,33.9451&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light"
+    <section style={{ width: `100vw`, height: `${frameHeight}px` }}>
+      <iframe
+        className="overflow-hidden fixed z-20 w-full border-0"
+        height={`${frameHeight}px`}
+        title="Test_Karlie"
+        src="https://experience.arcgis.com/experience/8758843303254380b2584e2047cd8831/"
+        //"https://tudelft.maps.arcgis.com/apps/Embed/index.html?webmap=e788f61aa7aa49dca367c8db8d934271&extent=34.6218,32.9125,36.4579,33.9451&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light"
+        //"https://tudelft.maps.arcgis.com/apps/instant/exhibit/index.html?appid=12679d3932ca414db83be414b7abf680&zoom=true&scale=true&disable_scroll=true"
+        //"https://tudelft.maps.arcgis.com/apps/Embed/index.html?webmap=e788f61aa7aa49dca367c8db8d934271&extent=34.6218,32.9125,36.4579,33.9451&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light"
         loading="lazy"
       ></iframe>
     </section>
