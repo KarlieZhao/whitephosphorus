ce52d3cadfab735cea772ce0f11a9d59d824fe41
Karlie Zhao <62089883+KarlieZhao@users.noreply.github.com>
2024-10-21 17:40:41 -0400

    layout tweaks

--- src/app/_components/window.tsx
+++ src/app/_components/window.tsx
@@ -15,10 +15,10 @@ export function DraggableWindow({ children, title = "", customeClassNameWindow, 
             <div className={`draggable-container`}>
                 <div className={`content w-full ${customeClassNameWindow}`}>
                     <div className="handle no-select">
-                        <div className="control-box close-box"><a className="control-box-inner"></a></div>
+                        {/* <div className="control-box close-box"><a className="control-box-inner"></a></div>
                         <div className="control-box zoom-box"><div className="control-box-inner"><div className="zoom-box-inner"></div></div></div>
-                        <div className="control-box windowshade-box"><div className="control-box-inner"><div className="windowshade-box-inner"></div></div></div>
-                        <h1 className="title text-2xl">{title}</h1>
+                        <div className="control-box windowshade-box"><div className="control-box-inner"><div className="windowshade-box-inner"></div></div></div> */}
+                        <h1 className="title block text-2xl">{title}</h1>
                     </div>
                     <div className={`inner h-full selectable ${customeClassNameInner}`}>
                         <div className={`mt-2 mb-5`}>
