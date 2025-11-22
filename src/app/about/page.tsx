b5233d180f6543e546a38dcc6700c0b96c0678ba
Karlie Zhao <62089883+KarlieZhao@users.noreply.github.com>
2025-11-21 21:57:14 -0500

    remove name

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -80,7 +80,7 @@ export default function Index() {
                         <a href="https://greensoutherners.org" target="_blank">Green Southerners</a> provided on-the-ground support, contributing footage and assisting with geolocation.
                         <br /> <br />
                         <span className="text-gray-100">Special thanks to:</span><br />
-                        Marc Schoonderbeek, Aleksandar Stancic, Chris Osieck, Wim Zwijnenburg, Hisham Younes, Tarek Ali Ahmad, Khodor Joujou, Ali Slayman, Karlie Zhao, Abbas Baalbaki, Ramzi Kaiss, Justin Salhani, Cosette Molijn, Carmen Joukhadar, William Christou, Alex Spoerndli, Jake Tacchi, X:@Easybakeovensz, X:@NemoAnno.
+                        Marc Schoonderbeek, Aleksandar Stancic, Chris Osieck, Wim Zwijnenburg, Hisham Younes, Tarek Ali Ahmad, Khodor Joujou, Ali Slayman, Karlie Zhao, Abbas Baalbaki, Ramzi Kaiss, Justin Salhani, Cosette Molijn, Carmen Joukhadar, William Christou, Alex Spoerndli, X:@Easybakeovensz, X:@NemoAnno.
                         <div className="mt-8">Satellite imagery provided by Planet Labs PBC under an educational license.</div>
                         <div className="mt-10 mb-16">Follow us on <a href="https://x.com/Phosphor_Abyad" target="_blank">Twitter</a> and <a href="https://bsky.app/profile/whitephosphorus.info" target="_blank">BlueSky</a>.</div>
                     </div>
