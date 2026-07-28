e4ed0c33a31114ebc4e04472d029c71dad3f432c
Baydoun <ahmbey94@gmail.com>
2026-07-28 23:13:56 +0200

    updated opening text
    updated opening text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Footer from "./_components/footer";
 
 
 export default function Index() {
-  const textToType = ["This public archive documents 285 white phosphorus strikes in South Lebanon and one in northern Israel between October 2023 and May 2026.", "Through spatial analysis and open-source investigation of over 700 images and videos, each strike was verified, geolocated, and chronolocated.", "Click anywhere to start."];
+  const textToType = ["This public archive documents 285 white phosphorus strikes in South Lebanon and one in northern Israel between October 2023 and May 2026.", "Through open source tools, more than 700 images and videos were collected and verified as evidence of white phosphorus use, the majority of which were geolocated and chronolocated.", "Click anywhere to start."];
   const [TypewriterFinished, setTypeWriterFinished] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
   const [forceStop, setForceStop] = useState<boolean>(false);
